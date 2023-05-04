53c36a66462a44c0cc9b7d16a7dedff8850f1972
Sage-tech <sierra.sage@icloud.com>
2023-05-03 18:51:37 -0700

    ok

--- portfolio-react/src/index.js
+++ portfolio-react/src/index.js
@@ -3,12 +3,9 @@ import React from 'react'
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
-
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-
 import './index.css'
 
 const root = createRoot(rootElement);
